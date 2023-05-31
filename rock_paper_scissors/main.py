7cadcb4f8ee020b12558599a3a4076e6716cd3b5
Piotr Kuligowski <pkuligowski99@gmail.com>
2023-05-31 21:12:53 +0200

    Add timer to the game

--- rock_paper_scissors/main.py
+++ rock_paper_scissors/main.py
@@ -1,4 +1,5 @@
 import cv2
+from time import time
 from cvzone.HandTrackingModule import HandDetector
 
 video_capture = cv2.VideoCapture(0)
@@ -10,6 +11,10 @@ video_capture.set(4, 480) # Height
 # 0.4453125 | 0.875 are the resizes for fx and fy
 
 detector = HandDetector(maxHands=1)
+timer = 0
+game_result = False
+game_start = False
+
 
 
 while True:
@@ -23,13 +28,25 @@ while True:
     # Track hands
     hands, img = detector.findHands(image_resized)
 
-    if hands:
-        hand = hands[0]
-        fingers_up = detector.fingersUp(hand)
+    if game_start:
+
+        if game_result is False:
+            timer = time() - initialTime
+            cv2.putText(image_background, str(int(timer)), (605, 400), cv2.FONT_HERSHEY_COMPLEX, 6, (0, 0, 0), 4)
+
+        if hands:
+            hand = hands[0]
+            fingers_up = detector.fingersUp(hand)
+            print(fingers_up)
 
     # Crop the resized video capture into the player position of the background
     image_background[148:568 , 852:1138] = image_resized
 
+    cv2.imshow("Rock! Paper! Scissors!", image_background)
+    key = cv2.waitKey(1)
+    if key == ord('s'):
+        game_start = True
+        initialTime = time()
 
-    cv2.imshow("Background", image_background)
-    cv2.waitKey(1)
+
+    
