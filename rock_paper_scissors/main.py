9b106122c28671ab65ce2639aaa469f7f638c02e
Piotr Kuligowski <pkuligowski99@gmail.com>
2023-05-31 22:18:52 +0200

    add AI making moves

--- rock_paper_scissors/main.py
+++ rock_paper_scissors/main.py
@@ -1,6 +1,8 @@
 import cv2
 from time import time
 from cvzone.HandTrackingModule import HandDetector
+from cvzone import overlayPNG
+import random
 
 video_capture = cv2.VideoCapture(0)
 video_capture.set(3, 640) # Width
@@ -29,15 +31,40 @@ while True:
     hands, img = detector.findHands(image_resized)
 
     if game_start:
-
         if game_result is False:
             timer = time() - initialTime
-            cv2.putText(image_background, str(int(timer)), (605, 400), cv2.FONT_HERSHEY_COMPLEX, 6, (0, 0, 0), 4)
+            cv2.putText(image_background, str(int(timer)), (590, 400), cv2.FONT_HERSHEY_COMPLEX, 6, (0, 0, 0), 4)
 
-        if hands:
-            hand = hands[0]
-            fingers_up = detector.fingersUp(hand)
-            print(fingers_up)
+            if timer > 3:
+                game_result = True
+                timer = 0
+
+                if hands:
+                    hand = hands[0]
+                    fingers_up = detector.fingersUp(hand)
+                    players_choice = sum(fingers_up)
+                    if players_choice == 0:
+                        # Rock
+                        print("Rock")
+                        pass
+                    elif players_choice > 0 and players_choice < 5:
+                        # Scissors
+                        print("Scissors")
+                        pass
+                    elif players_choice == 5:
+                        # Paper
+                        print("Paper")
+                        pass
+                    
+                    options = ["rock", "paper", "scissors"]
+                    random_option = random.choice(options)
+                    image_AI = cv2.imread(f'Images/{random_option}.png', cv2.IMREAD_UNCHANGED)
+                    image_AI = cv2.resize(image_AI, (241, 241), interpolation=cv2.INTER_AREA)
+                    image_background = overlayPNG(image_background, image_AI, (212, 214))
+
+
+    if game_result:
+        image_background = overlayPNG(image_background, image_AI, (212, 214))
 
     # Crop the resized video capture into the player position of the background
     image_background[148:568 , 852:1138] = image_resized
@@ -47,6 +74,7 @@ while True:
     if key == ord('s'):
         game_start = True
         initialTime = time()
+        game_result = False
 
 
     
