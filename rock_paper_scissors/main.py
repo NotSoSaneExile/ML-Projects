87df4fecd3f22cd2d5278878b5953732d3a5e15d
Piotr Kuligowski <pkuligowski99@gmail.com>
2023-05-31 19:43:25 +0200

    create a background for game with the player in it

--- rock_paper_scissors/main.py
+++ rock_paper_scissors/main.py
@@ -1,8 +1,12 @@
 import cv2
 
 video_capture = cv2.VideoCapture(0)
-video_capture.set(3, 1024) # Width
-video_capture.set(4, 768) # Height
+video_capture.set(3, 640) # Width
+video_capture.set(4, 480) # Height
+
+# 1138, 568 | 852, 148 background params
+
+# 0.4453125 | 0.875 are the resizes for fx and fy
 
 while True:
 
@@ -10,6 +14,11 @@ while True:
     image_background = cv2.imread("Images/background.png")
 
     success, image = video_capture.read()
-    cv2.imshow("Rock - Paper - Scissors", image)
+
+    image_resized = cv2.resize(image, (0, 0), None, 0.875, 0.875)
+    image_resized = image_resized[:,137:423] # 137:423 or 138:424
+
+    image_background[148:568 , 852:1138] = image_resized
+
     cv2.imshow("Background", image_background)
     cv2.waitKey(1)
